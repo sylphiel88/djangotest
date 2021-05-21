e2c3aaf14a7483e8dadecfb9db4d6b8597ec7d5b
sylphiel88 <jihmels@gmail.com>
2021-05-21 09:43:09 +0200

    color picker final

--- portfolio/portfolioa/views.py
+++ portfolio/portfolioa/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'name':'Jan'})
+    if 'color' in request.COOKIES.keys():
+        color = request.COOKIES['color']
+        color2 = request.COOKIES['color2']
+    else:
+        resp = HttpResponse('')
+        color = '#ff0000'
+        color2 = '#dd0000'
+        resp.set_cookie('color',color)
+        resp.set_cookie('color2',color2)
+    return render(request, 'home.html', {'name':'Jan', 'color':color, 'color2':color2})
 
 def projects(request):
-    return render(request, 'projects.html')
+    color = request.COOKIES['color']
+    color2 = request.COOKIES['color2']
+    return render(request, 'projects.html', {'color':color, 'color2':color2})
