f0f9f16793493837d4a57e98f17ab628f073268d
sylphiel88 <jihmels@gmail.com>
2021-05-21 10:10:57 +0200

    color picker text inv color

--- portfolio/portfolioa/views.py
+++ portfolio/portfolioa/views.py
@@ -9,13 +9,16 @@ def home(request):
     if 'color' in request.COOKIES.keys():
         color = request.COOKIES['color']
         color2 = request.COOKIES['color2']
+        colorbw = request.COOKIES['colorbw']
     else:
         resp = HttpResponse('')
         color = '#9DCC1E'
         color2 = '#4F660F'
+        colorbw = '#FFFFFF'
         resp.set_cookie('color',color)
         resp.set_cookie('color2',color2)
-    return render(request, 'home.html', {'name':'Jan', 'color':color, 'color2':color2})
+        resp.set_cookie('color',colorbw)
+    return render(request, 'home.html', {'name':'Jan', 'color':color, 'color2':color2, 'colorbw':colorbw})
 
 def projects(request):
     if 'color' in request.COOKIES.keys():
