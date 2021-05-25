ad1209459edcb7386559560455248c0f179f56e0
sylphiel88 <jihmels@gmail.com>
2021-05-25 15:27:45 +0200

    settings auf gitignore

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -83,8 +83,9 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aquarium',
+        'USER': 'postgres'
     }
 }
 
